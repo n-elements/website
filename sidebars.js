15180633e8acf78b652dfb5299e999a8b392106e
Mattia Astorino <astorino.mattia@gmail.com>
2020-06-06 18:34:39 +0200

    chore: improve installation doc

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,10 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['getting-started/introduction', 'getting-started/installation'],
-    Features: ['mdx'],
+    'Getting Started': [
+      'getting-started/introduction',
+      'getting-started/installation',
+      'getting-started/usage',
+    ],
+    Elements: ['elements/button'],
   },
 };
