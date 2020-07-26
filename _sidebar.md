00e10dcda2f92d701c2e83df114248f0ff8febba
Mattia Astorino <astorino.mattia@gmail.com>
2020-07-26 15:06:29 +0200

    chore: try dcosify

---
+++ _sidebar.md
@@ -0,0 +1,2 @@
+* [Home](/pages/README.md)
+* [Guide](docs/getting-started/introduction.md)
--- src/components/Spacer/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Spacer = ({ height = '16px' }) => (
-  <br style={{ lineHeight: height, }} />
-);
